571e1b36ac17cf6005bc96de298d378f1c099693
brewermawa <brewermawa@gmail.com>
2024-09-18 18:13:28 -0600

    Added color fiels to product

--- apps/catalogue/models.py
+++ apps/catalogue/models.py
@@ -1,4 +1,5 @@
-from oscar.apps.catalogue.abstract_models import AbstractCategory
+from django.db import models
+from oscar.apps.catalogue.abstract_models import AbstractCategory, AbstractProduct
 
 
 class Category(AbstractCategory):
@@ -15,6 +16,20 @@ class Category(AbstractCategory):
             
     def get_num_products(self):
         return self.get_cat_num_products()
+    
+
+class Product(AbstractProduct):
+    COLOR_CHOICES = [
+        ("grn", "Green"),
+        ("gry", "Grey"),
+        ("blu", "Blue"),
+        ("blk", "Black"),
+        ("yel", "Yellow"),
+        ("red", "Red"),
+        ("wht", "White"),
+    ]
+
+    color = models.CharField(max_length=3, choices=COLOR_CHOICES, default="wht")
 
 
 from oscar.apps.catalogue.models import * 
